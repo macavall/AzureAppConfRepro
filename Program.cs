294ba75a192d9cf9d5244a7d3b5cda1df75f4be8
Matt <macavall@microsoft.com>
2025-05-09 18:20:15 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,90 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.FeatureManagement;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = FunctionsApplication.CreateBuilder(args);
+
+        // Connect to Azure App Configuration
+        builder.Configuration.AddAzureAppConfiguration(options =>
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_APPCONFIG_CONNECTION_STRING") ??
+                throw new InvalidOperationException("The environment variable 'AZURE_APPCONFIG_CONNECTION_STRING' is not set or is empty.");
+            options.Connect(connectionString)
+                   // Load all keys that start with `TestApp:` and have no label
+                   .Select("TestApp")
+                   // Reload configuration if any selected key-values have changed.
+                   // Use the default refresh interval of 30 seconds. It can be overridden via AzureAppConfigurationRefreshOptions.SetRefreshInterval.
+                   .ConfigureRefresh(refreshOptions =>
+                   {
+                       refreshOptions.RegisterAll();
+                       refreshOptions.SetRefreshInterval(TimeSpan.FromSeconds(5));
+                   });
+        });
+
+        builder.Services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights()
+            .AddHostedService<AzureAppConfigRefreshService>()
+            .AddAzureAppConfiguration()
+            .AddFeatureManagement();
+
+        builder.UseAzureAppConfiguration();
+        builder.ConfigureFunctionsWebApplication();
+
+        //builder
+        //.UseWhen<StampHttpHeaderMiddleware>((context) =>
+        //{
+        //    Console.WriteLine(context.FunctionDefinition.InputBindings.Values.First(a => a.Type.EndsWith("Trigger")).Type);
+        //    // We want to use this middleware only for http trigger invocations.
+        //    return context.FunctionDefinition.InputBindings.Values
+        //                    .First(a => a.Type.EndsWith("Trigger")).Type == "httpTrigger";
+        //});
+
+        builder.Build().Run();
+    }
+
+    internal sealed class AzureAppConfigRefreshService : BackgroundService
+    {
+        private readonly IEnumerable<IConfigurationRefresher> _refreshers;
+
+        public AzureAppConfigRefreshService(IConfigurationRefresherProvider refresherProvider)
+        {
+            _refreshers = refresherProvider.Refreshers;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    foreach (var refresher in _refreshers)
+                    {
+                        await refresher.TryRefreshAsync(stoppingToken);
+                    }
+
+                    await Task.Delay(0);
+                }
+                catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("AzureAppConfig has been stopped");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ durable.cs
@@ -0,0 +1,65 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppConfigRefresh
+{
+    public class durable
+    {
+        private readonly IConfiguration _configuration;
+
+        public durable(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Function(nameof(durable))]
+        public async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] TaskOrchestrationContext context)
+        {
+            ILogger logger = context.CreateReplaySafeLogger(nameof(durable));
+            logger.LogInformation("Saying hello.");
+            var outputs = new List<string>();
+
+            // Replace name and input with values relevant for your Durable Functions Activity
+            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
+            //outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
+            //outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [Function(nameof(SayHello))]
+        public string SayHello([ActivityTrigger] string name, FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger("SayHello");
+            logger.LogInformation("Saying hello to {name}.", _configuration["TestApp"]);
+            return $"Hello {name}!";
+        }
+
+        [Function("durable_HttpStart")]
+        public async Task<HttpResponseData> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            [DurableClient] DurableTaskClient client,
+            FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger("durable_HttpStart");
+
+            // Function input comes from the request content.
+            string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
+                nameof(durable));
+
+            logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
+
+            // Returns an HTTP 202 response with an instance management payload.
+            // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
+            return await client.CreateCheckStatusResponseAsync(req, instanceId);
+        }
+    }
+}
