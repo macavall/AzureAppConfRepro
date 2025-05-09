15374d9de50d0e745b6d1f35b27967bdd4a3fa55
Matt <macavall@microsoft.com>
2025-05-09 18:32:38 -0500

    first

--- durable.cs
+++ durable.cs
@@ -28,8 +28,8 @@ namespace AppConfigRefresh
 
             // Replace name and input with values relevant for your Durable Functions Activity
             outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
-            //outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
-            //outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
+            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "London"));
 
             // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
