6a98e0bb36fee1987393bdc52a558d1db2a1c1ee
Matt <macavall@microsoft.com>
2025-05-12 12:10:01 -0500

    first

---
+++ AppConfigService.cs
@@ -0,0 +1,30 @@
+﻿using Castle.Core.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppConfigRefresh
+{
+    public class AppConfigService : IAppConfigService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConfigurationRefresher _refresher;
+        public AppConfigService(IConfiguration configuration, IConfigurationRefresher refresher)
+        {
+            _configuration = configuration;
+            _refresher = refresher;
+        }
+        public async Task RefreshConfigurationAsync(CancellationToken cancellationToken)6 
+        {
+            await _refresher.TryRefreshAsync(cancellationToken);
+        }
+        public string GetValue(string key)
+        {
+            return _configuration[key];
+        }
+    }
+}
---
+++ IAppConfigService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppConfigRefresh
+{
+    public interface IAppConfigService
+    {
+        public Task RefreshConfigurationAsync(CancellationToken cancellationToken);
+
+        public string GetValue(string key);
+    }
+}
