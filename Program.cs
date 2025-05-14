d3a3546cf201773a24822d4c207b2e5de2fd0c43
Matt <macavall@microsoft.com>
2025-05-14 13:11:34 -0500

    first

--- Program.cs
+++ Program.cs
@@ -11,54 +11,7 @@ using Microsoft.FeatureManagement;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using System.Collections.Generic;
 using System.Threading;
-
-public static class AzureAppConfigurationRefreshExtensions2
-{
-    public static IFunctionsWorkerApplicationBuilder UseAzureAppConfiguration2(this IFunctionsWorkerApplicationBuilder builder)
-    {
-        IEnumerable<IConfigurationRefresher> refreshers = (ServiceProviderServiceExtensions.GetService<IConfigurationRefresherProvider>(builder.Services.BuildServiceProvider()) ?? throw new InvalidOperationException("Unable to find the required services. Please add all the required services by calling 'IServiceCollection.AddAzureAppConfiguration()' in the application startup code.")).Refreshers;
-        if (refreshers != null && refreshers.Count() > 0)
-        {
-            builder.UseMiddleware<AzureAppConfigurationRefreshMiddleware2>();
-        }
-        return builder;
-    }
-}
-
-internal class AzureAppConfigurationRefreshMiddleware2 : IFunctionsWorkerMiddleware
-{
-    private static readonly long MinimumRefreshInterval = TimeSpan.FromSeconds(1.0).Ticks;
-
-    private long _refreshReadyTime = DateTimeOffset.UtcNow.Ticks;
-
-    private IEnumerable<IConfigurationRefresher> Refreshers { get; }
-
-    public AzureAppConfigurationRefreshMiddleware2(IConfigurationRefresherProvider refresherProvider)
-    {
-        Refreshers = refresherProvider.Refreshers;
-    }
-
-    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
-    {
-        long ticks = DateTimeOffset.UtcNow.Ticks;
-        long num = Interlocked.Read(ref _refreshReadyTime);
-        if (num <= ticks && Interlocked.CompareExchange(ref _refreshReadyTime, ticks + MinimumRefreshInterval, num) == num)
-        {
-            using (ExecutionContext.SuppressFlow())
-            {
-                foreach (IConfigurationRefresher refresher in Refreshers)
-                {
-                    Task.Run(() => refresher.TryRefreshAsync());
-                }
-            }
-        }
-        
-        // OLD VERSION
-        //await next(context).ConfigureAwait(continueOnCapturedContext: false);
-
-        await next(context); //.ConfigureAwait(true); // continueOnCapturedContext: false);
-    }
-}
+using AppConfigRefresh;
 
 public class Program
 {
@@ -93,46 +46,6 @@ public class Program
         builder.UseAzureAppConfiguration2();
         builder.ConfigureFunctionsWebApplication();
 
-        //builder
-        //.UseWhen<StampHttpHeaderMiddleware>((context) =>
-        //{
-        //    Console.WriteLine(context.FunctionDefinition.InputBindings.Values.First(a => a.Type.EndsWith("Trigger")).Type);
-        //    // We want to use this middleware only for http trigger invocations.
-        //    return context.FunctionDefinition.InputBindings.Values
-        //                    .First(a => a.Type.EndsWith("Trigger")).Type == "httpTrigger";
-        //});
-
         builder.Build().Run();
-    }
-
-    internal sealed class AzureAppConfigRefreshService : BackgroundService
-    {
-        private readonly IEnumerable<IConfigurationRefresher> _refreshers;
-
-        public AzureAppConfigRefreshService(IConfigurationRefresherProvider refresherProvider)
-        {
-            _refreshers = refresherProvider.Refreshers;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                try
-                {
-                    foreach (var refresher in _refreshers)
-                    {
-                        await refresher.TryRefreshAsync(stoppingToken);
-                    }
-
-                    await Task.Delay(0);
-                }
-                catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
-                {
-                    Console.WriteLine("AzureAppConfig has been stopped");
-                    break;
-                }
-            }
-        }
     }
 }
